0061cf5270dab806a9e7fbbdcbfe7a2eed5eec8a
ardel-c <ardelia.christina@gmail.com>
2021-04-11 22:07:23 +0700

    add automation test

--- news_scraper/api/tests.py
+++ news_scraper/api/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .models import News
 
 # Create your tests here.
+class ScrapeTestCase(TestCase):
+    """This class defines the testing for the Scraping Capability and News model."""
+    def test_scrape(self):
+        """Insert keyword for test here """
+        keyword = ["COVID", "Libur", "Deloitte", "Jakarta Barat", "Banjir Bandang"]
+        for i in range(len(keyword)):
+            response = self.client.get("/v1/news/", params = {"keyword": keyword[i]})
+            self.assertEqual(response.status_code, 200)
+
